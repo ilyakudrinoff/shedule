f3e4f6b913d544f7069d8c8e37ba0dae722d4aa8
ilya <ilya@ilya.ru>
2023-01-02 12:26:55 +0700

    Спроектирована БД. Сформирована Админская панель.

--- purposes/admin.py
+++ purposes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Purposes, Tasks
 
-# Register your models here.
+
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('purpose', 'name', 'description', 'deadline', 'date_complete',)
+    search_fields = ('name', 'purpose')
+    list_filter = ('deadline', 'date_complete', 'purpose',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Purposes)
+class PurposesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'description', 'deadline', 'date_complete',)
+    search_fields = ('name',)
+    list_filter = ('deadline', 'date_complete', 'user',)
+    empty_value_display = '-пусто-'
--- purposes/models.py
+++ purposes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Purposes(models.Model):
+    name = models.CharField('Цель', max_length=200)
+    description = models.TextField('Описание цели', blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    deadline = models.DateTimeField('Дедлайн')
+    date_complete = models.DateTimeField('Дата комплита', blank=True, null=True)
+
+    class Meta:
+        ordering = ('deadline',)
+        verbose_name = 'Список целей'
+        verbose_name_plural = 'Список целей'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Tasks(models.Model):
+    name = models.CharField('Задача', max_length=200)
+    description = models.TextField('Описание задачи', blank=True, null=True)
+    deadline = models.DateTimeField('Дедлайн')
+    date_complete = models.DateTimeField('Дата комплита', blank=True, null=True)
+    purpose = models.ForeignKey(Purposes, on_delete=models.CASCADE, verbose_name='Цель')
+
+    class Meta:
+        ordering = ('deadline',)
+        verbose_name = 'Список задач'
+        verbose_name_plural = 'Список задач'
+
+    def __str__(self):
+        return f'{self.name}'
